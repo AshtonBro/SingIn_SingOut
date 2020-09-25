a9d7b118876b1d98cf4419159d210800a0c7af3d
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-09-25 21:47:52 +0500

    js Add EventListeners

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const signInBtn = document.getElementById("signIn");
+const signUpBtn = document.getElementById("signUp");
+const fistForm = document.getElementById("form1");
+const secondForm = document.getElementById("form2");
+const container = document.querySelector(".container");
+
+signInBtn.addEventListener("click", () => {
+    container.classList.remove("right-panel-active");
+});
+
+signUpBtn.addEventListener("click", () => {
+    container.classList.add("right-panel-active");
+});
+
+fistForm.addEventListener("submit", (event) => event.preventDefault());
+secondForm.addEventListener("submit", (event) => event.preventDefault());
